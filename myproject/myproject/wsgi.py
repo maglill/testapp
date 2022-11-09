411d9fc0ea876d397488b1b783518c8cbd1f6b93
maglill <93708454+maglill@users.noreply.github.com>
2022-11-09 10:54:54 +0900

    Update wsgi.py

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+#OSモジュールのインポート → Python上で書かれたコードをどのOSでも動くようにするため。
 
 from dj_static import Cling
+#Clingをimport。
 from django.core.wsgi import get_wsgi_application
+#get_wsgi_applicationをimport。
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
